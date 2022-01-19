8456d0f5d32a562c103d3822d06eb99ac1f52f16
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2022-01-19 12:08:35 +0700

    fix: исправил ошибку обнаружени файла если в ссылке есть пробелы

--- classes/services/helper.php
+++ classes/services/helper.php
@@ -6,6 +6,8 @@ class helper
 {
     public static function remote_file_exists($url): bool
     {
+        $url = str_replace(" ", '%20', $url);
+
         return (bool) preg_match('~HTTP/1\.\d\s+200\s+OK~', @current(get_headers($url)));
     }
 }
