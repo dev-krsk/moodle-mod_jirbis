79f9871d56f4de4cb83eba4cfbef4ad3273c78d0
Yuriy Yurinskiy <yuriyyurinskiy@yandex.ru>
2022-01-19 12:26:11 +0700

    fix: отключил проверку файла на удаленном сервере
    Работает не стабильно, сервер через раз возвращает коды 503, 400 или 200

--- classes/services/helper.php
+++ classes/services/helper.php
@@ -6,8 +6,10 @@ class helper
 {
     public static function remote_file_exists($url): bool
     {
-        $url = str_replace(" ", '%20', $url);
-
-        return (bool) preg_match('~HTTP/1\.\d\s+200\s+OK~', @current(get_headers($url)));
+        return true;
+// Работает не стабильно, сервер через раз возвращает коды 503, 400 или 200
+//        $url = str_replace(" ", '%20', $url);
+//
+//        return (bool) preg_match('~HTTP/1\.\d\s+200\s+OK~', @current(get_headers($url)));
     }
 }
